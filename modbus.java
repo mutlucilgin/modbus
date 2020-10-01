2b35e900f7f4102637af6415ec370a288cd7e92d
crazy <mutlu-cilgin@hotmail.com>
2020-10-01 11:48:13 +0300

    server

--- modbus.java
+++ modbus.java
@@ -2,6 +2,8 @@
 import java.text.SimpleDateFormat;
 import java.io.*;
 import java.util.*;
+
+import de.re.easymodbus.modbusclient.ModbusClient;
 import de.re.easymodbus.server.*;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -9,7 +11,8 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 
 public class modbus {
-    static int period=200;              // "num" değerinin artma periyodu
+    static int period = 200; // "num" değerinin artma periyodu
+
     public static void main(String[] args) throws IOException {
 
         Timer timer = new Timer();
@@ -23,17 +26,20 @@ public class modbus {
 class MyTimerTask extends TimerTask {
     static HSSFWorkbook workbook;
     static HSSFSheet sheet;
-    static int num = 0;                 // modbus'a ait holding register adresinde bulunacak olan veri
-    int counter = 1;                    // her periyotta çalışan metodun toplam çalışma 
-    int countLoop=0;                    // tekrar eden döngü sayısı 
-    int stopLoop = 2;                  // tekrar edecek toplam döngü sayısı
-    int max_num =5;                   // "num" sayısının çıkabileceği max değer(Bu değere ulaştıktan sonra "num" sayısı sıfırlanmaktadır.)
+    static int num = 0;                         // modbus'a ait holding register adresinde bulunacak olan veri
+    int counter = 1;                            // her periyotta çalışan metodun toplam çalışma
+    int countLoop = 0;                          // tekrar eden döngü sayısı
+    int stopLoop = 5;                           // tekrar edecek toplam döngü sayısı
+    int max_num = 5;                            // "num" sayısının çıkabileceği max değer(Bu değere ulaştıktan sonra "num"
+                                                // sayısı sıfırlanmaktadır.)
 
-    ModbusServer modbusServer = new ModbusServer();  // oluşturulan modbus server
+    ModbusServer modbusServer = new ModbusServer();             // oluşturulan modbus server
+    ModbusClient client = new ModbusClient("127.0.0.1", 502);   // modbusServer'ı durdurmak için oluşturuldu.
     static Timer myTimer;
+
     MyTimerTask(Timer timer) {
-        myTimer=timer;
-        modbusServer.setPort(502);// Note that Standard Port for Modbus TCP communication is 502
+        myTimer = timer;
+        modbusServer.setPort(502);                              // Note that Standard Port for Modbus TCP communication is 502
         try {
             workbook = new HSSFWorkbook();
             sheet = workbook.createSheet("Sample Sheet");
@@ -43,7 +49,7 @@ class MyTimerTask extends TimerTask {
             cell = row.createCell(1);
             cell.setCellValue("Value");
 
-            //modbusServer.coils[1] = true;
+            // modbusServer.coils[1] = true;
             modbusServer.Listen();
             System.out.println("listening start");
         } catch (IOException e) {
@@ -57,21 +63,22 @@ class MyTimerTask extends TimerTask {
         Date date = new Date();
         SimpleDateFormat DateFor = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
         String stringDate = DateFor.format(date);
-        
+
         System.out.println(" Time = " + stringDate + "    value = " + num);
-        modbusServer.holdingRegisters[1] = num;                                 //modbus server'a ait holding register adresine veri yerleştiriliyor
+        modbusServer.holdingRegisters[1] = num; // modbus server'a ait holding register adresine veri yerleştiriliyor
         modbusServer.holdingRegisters[2] = num;
         modbusServer.holdingRegisters[3] = num;
 
-        // holding register adresinde bulunan num değeri, her periyotta zamanı ile birlikte excel dosyasına yazdırılmaktadır.
+        // holding register adresinde bulunan num değeri, her periyotta zamanı ile
+        // birlikte excel dosyasına yazdırılmaktadır.
         HSSFRow row = sheet.createRow(counter);
         Cell cell = row.createCell(0);
         cell.setCellValue(stringDate);
         cell = row.createCell(1);
         cell.setCellValue(num);
-        if(counter>=max_num){
-            num=0;
-            counter=0;
+        if (counter >= max_num) {
+            num = 0;
+            counter = 0;
             countLoop++;
         }
         counter++;
@@ -83,7 +90,9 @@ class MyTimerTask extends TimerTask {
                 workbook.close();
                 System.out.println("kapandı");
                 modbusServer.StopListening();
-                System.out.println("kapandı");
+                client.Connect();               // Neden bilmiyorum StopListening yapınca program sonlanmıyor ama bu şekilde
+                                                // yeni bir
+                                                // bağlantı oluşunca kendini kapatıyor.
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
